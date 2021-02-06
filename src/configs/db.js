3696df29d5f0b25c4c4419aabc7af94fbe88d033
DiaryKyosuke <fendianwar36@gmail.com>
2021-02-06 11:25:18 +0700

    update for deploy db

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2')
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
